6ee8841bf61bb6036337d788e0b183988dbf1956
saicharan0312 <46310435+saicharan0312@users.noreply.github.com>
2020-09-15 18:29:15 +0530

    Update FirstPositive.java

--- FirstPositive.java
+++ FirstPositive.java
@@ -1,5 +1,5 @@
 /* Given an array of integers, find the first missing positive integer in linear time and constant space. In other words,
- find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well. */
+ find the lowest positive integer that does  not exist in the given array. The array can contain duplicates and negative numbers as well. */
  
  
 import java.io.*;
